10cb19b4b067f45e5759b4446c66de21026b329d
Annette Maza <nettymaza@gmail.com>
2018-05-23 22:57:14 -0600

    updating

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ItemCard from './ItemCard'
 // import { addLike } from '../actions/items';
 
 const DetailPage = ({ item })=> {
