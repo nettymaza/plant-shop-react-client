c385fb1a37d8c58479efb1fafd8f1e9780779c46
Annette Maza <nettymaza@gmail.com>
2018-05-23 22:47:50 -0600

    like feature works only item clicked is incremented

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
+import ItemCard from './ItemCard'
 // import { addLike } from '../actions/items';
 
 const DetailPage = ({ item })=> {
@@ -9,7 +9,6 @@ const DetailPage = ({ item })=> {
       <h3>{item.name}</h3>
       <p>{item.info}</p>
       <img src={item.img_url} alt={item.name}/>
-      <button>Likes: {item.likes_count}</button>
     </div>
   );
 }
@@ -27,41 +26,4 @@ const mapStateToProps = (state, ownProps) => {
 export default connect(mapStateToProps)(DetailPage);
 
 
-  // <button onClick={addLike}>Likes: {item.likes_count}</button>
-
-
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { addLike } from '../actions/items';
-//
-// class DetailPage extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       likes: 0
-//     }
-//   }
-//
-//   render() {
-//     return(
-//       <div className="ItemCard">
-//         <h3>{this.props.item.name}</h3>
-//         <p>{this.props.item.info}</p>
-//         <img src={this.props.item.img_url} alt={this.props.item.name}/>
-//         <button onClick={addLike}>Likes: {this.props.item.likes_count}</button>
-//       </div>
-//     )
-//   }
-// }
-//
-// const mapStateToProps = (state, ownProps) => {
-//   const item = state.items.find(item => item.id === +ownProps.match.params.itemId)
-//
-//   if (item) {
-//     return { item }
-//   } else {
-//     return { item: {} }
-//   }
-// }
-//
-// export default connect(mapStateToProps)(DetailPage);
+// <button onClick={addLike}>Likes: {item.likes_count}</button>
