ae04b998188cd54c08800c5d0711d036377cdbc3
Annette Maza <nettymaza@gmail.com>
2018-05-29 10:19:33 -0600

    Worked with Array methods

---
+++ src/components/ItemCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ItemCard extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        likes: 0
+      }
+  }
+
+  increaseLike = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+
+  render() {
+    return(
+      <button onClick={this.increaseLike}>Likes: {this.state.likes}</button>
+    );
+  }
+}
+
+export default ItemCard;
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+// import { Link } from 'react-router-dom';
+import { addLike } from '../actions/items'
 
-const ItemsList = ({ items }) => {
+const ItemsList = ({ items, addLike, totalLikes }) => {
 
-  const renderItems = items.map(item =>
-    <Link key={item.id} to={`/items/${item.id}`}>
+
+const filteredItems = items.filter( item => item.likes_count > 20)
+const sortedItems = filteredItems.sort(function(a, b) {
+  return(b.likes_count - a.likes_count)
+})
+
+  const renderItems = filteredItems.map(item =>
       <div className="ItemList">
         <h3>{item.name}</h3>
+        <h4>{item.likes_count}</h4>
+        <button onClick={() => addLike(item.id)}>Like</button>
       </div>
-    </Link>
   );
 
   return (
     <div>
+      <h1>Total Likes: {totalLikes}</h1>
       {renderItems}
     </div>
   );
 };
 
-export default ItemsList;
+const mapStateToProps = state => {
+  const totalLikes = state.items.reduce((accum, item) => {
+    return item.likes_count + accum
+  }, 0)
+  return {
+    totalLikes: totalLikes
+  }
+}
+
+export default connect(mapStateToProps, {addLike})(ItemsList);
